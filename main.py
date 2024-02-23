e3651a51655a4c81c1f72903f8be0af7dca66ecb
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2024-02-23 11:47:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
-import nextcord, argparse
+import nextcord
 from nextcord.ext import commands
 from dotenv import load_dotenv
 from os import environ
-
-Parser = argparse.ArgumentParser()
-for i in ["question", "reply"]:
-    Parser.add_argument(f"--{i}", required=True)
 
 Bot = commands.Bot(intents=nextcord.Intents.all())
 
 def main():
     load_dotenv()
-    global Args
-    Args = Parser.parse_args()
+
+    global Question, Reply
+    Question, Reply = __import__("sys").argv[1:]
     Bot.run(environ.get("TOKEN"))
 
 @Bot.event
 async def on_ready():
-    question = f"QOTD: {Args.question}"
+    qotd = f"QOTD: {Question}"
     await (
         await (
-            await Bot.get_channel(int(environ.get("CHANNEL"))).send(f"{question} @everyone")
-        ).create_thread(name=question)
-    ).send(Args.reply)
+            await Bot.get_channel(int(environ.get("CHANNEL"))).send(f"{qotd} @everyone")
+        ).create_thread(name=qotd)
+    ).send(Reply)
     await Bot.close()
 
 
